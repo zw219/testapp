43b56349c4a93090a1f04f5d6b56762662e30d8a
Jake Vestal <23266745+JakeVestal@users.noreply.github.com>
2021-11-23 15:22:20 -0500

    Update app.py

--- testapp/app.py
+++ testapp/app.py
@@ -2,7 +2,7 @@ import dash
 from dash import html
 
 app = dash.Dash(__name__)
-app.layout = html.Div([html.Img(src='assets/tvol_banner.png')])
+app.layout = html.Div([html.Img(src='assets/rollingoutloud.png')])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
