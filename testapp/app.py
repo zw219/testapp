a90d704696e68e8fe63867bbe5bfeef37440eb24
Jake Vestal <23266745+JakeVestal@users.noreply.github.com>
2021-11-29 10:45:56 -0500

    Update app.py

--- testapp/app.py
+++ testapp/app.py
@@ -1,6 +1,15 @@
 import dash
 from dash import html
 
+# Import the os module
+import os
+
+# Get the current working directory
+cwd = os.getcwd()
+
+# Print the current working directory
+print("Current working directory: {0}".format(cwd))
+
 app = dash.Dash(__name__)
 app.layout = html.Div([html.Img(src='assets/rollingoutloud_banner.png')])
 
