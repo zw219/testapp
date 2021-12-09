e84ae9f0695090654b9930be1c5301d107d36209
JakeVestal <jmvestal@gmail.com>
2021-12-09 16:25:54 -0500

    ready for rebex

--- testapp/__init__.py
+++ testapp/__init__.py
@@ -1,9 +1,17 @@
 # App modules
-import os
-
 from testapp.github_info_header import github_info_header
 
-import platform
-platform.uname()
+import os
+app_data_path = os.getenv("TESTAPP_DATA_PATH")
+
+if app_data_path is None:
+    raise Exception("You need to add the path to your data directory to the APP_DATA_PATH environmental variable!")
+
+
+can_cd_to_data_path = os.system("cd " + app_data_path)
+
+if can_cd_to_data_path != 0:
+    raise Exception("Cannot cd to " + app_data_path +
+                    ". Please create the directory or double-check your TESTAPP_DATA_PATH entry.")
 
 
