ae1a3c8a8cba9d61ceedca55640ee1c8b74c858c
JakeVestal <jmvestal@gmail.com>
2022-01-25 15:47:46 -0500

    switch

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Dash(__name__)
 server = app.server
 app.layout = html.Div([
     github_info_header(),
-    html.Img(src="assets/cute_ferret.jpeg")
+    html.Img(src="assets/burb.jpeg")
 ])
 
 if __name__ == '__main__':
