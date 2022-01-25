5c711cf4ab963ec89e6462cf879b564c205a0096
JakeVestal <jmvestal@gmail.com>
2022-01-25 15:17:01 -0500

    ready for rebex

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from testapp import *
 app = Dash(__name__)
 app.layout = html.Div([
     github_info_header(),
-    html.Img(src="assets/tvol_banner.png")
+    html.Img(src="assets/burb.jpeg")
 ])
 
 if __name__ == '__main__':
