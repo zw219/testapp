c8e9fcc3fd391df4dfabf94ddea1bd861a1926c5
zw219 <zw219@duke.edu>
2022-02-19 13:04:05 -0500

    .

--- serve_testapp.bat
+++ serve_testapp.bat
@@ -1,4 +1,4 @@
 cd C:\Users\vcm\Desktop\testapp
-git pull https://%TESTAPP_GIT_PAT%@github.com/JakeVestal/testapp.git
+git pull https://%TESTAPP_GIT_PAT%@github.com/zw219/testapp.git
 venv\Scripts\python.exe -m pip install -r requirements.txt
 venv\Scripts\python.exe server.py
